13c8baab8f04f5f83d3c6db0696c2948b1ab5358
throwingbits <64206466+throwingbits@users.noreply.github.com>
2024-02-02 02:19:29 +0100

    Added VadapavKnownDirectories

---
+++ src/Vadapav/VadapavKnownDirectories.cs
@@ -0,0 +1,16 @@
+﻿namespace Vadapav
+{
+    /// <summary>
+    /// Contains known vadapav directory GUID's. Beware they CAN change!
+    /// </summary>
+    public static class VadapavKnownDirectories
+    {
+        public static Guid RootDirectory = new Guid("11111111-1111-1111-1111-111111111111");
+        public static Guid AnimeDirectory = new Guid("d29d69f1-dc2a-4573-9959-45a765406e4d");
+        public static Guid BollywoodDirectory = new Guid("689371f2-8240-48d3-8ff5-2d7f31024304");
+        public static Guid DocumentariesDirectory = new Guid("251df62a-a79f-4e32-9765-d8c80f5a92b6");
+        public static Guid MoviesDirectory = new Guid("f36be06f-8edd-4173-99df-77bc4c7c2626");
+        public static Guid RecentDirectory = new Guid("7f49cd8a-ce5a-4547-9869-1c8e6783cfe7");
+        public static Guid TVDirectory = new Guid("28dc7aeb-902b-4824-8be2-fa1e4f20383c");
+    }
+}
