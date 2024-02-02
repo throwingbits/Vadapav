23de3cbe2bc3d67c6bdf65e4f6291072c149784e
throwingbits <64206466+throwingbits@users.noreply.github.com>
2024-02-02 10:25:59 +0100

    Draft

---
+++ src/Vadapav/DirectoryIterator.cs
@@ -0,0 +1,20 @@
+﻿using Vadapav.Models;
+using Vadapav.Models.Iteration;
+
+namespace Vadapav
+{
+    public interface IDirectoryIterator
+    {
+        Task<DirectoryIterationResult> IterateAsync();
+    }
+
+    internal class DirectoryIterator
+    {
+        private readonly VadapavDirectory _directory;
+
+        public DirectoryIterator(VadapavDirectory directory)
+        {
+            _directory = directory;
+        }
+    }
+}
---
+++ src/Vadapav/Models/Iteration/DirectoryIterationResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Vadapav.Models.Iteration
+{
+    public class DirectoryIterationItem<TVadapavModel>
+        where TVadapavModel : VadapavItem
+    {
+        public string Path { get; set; }
+    }
+
+    public class DirectoryIterationResult
+    {
+        public int Count { get; set; } = 0;
+
+        // public List<DirectoryIterationItem> Items { get; set; } = [];
+    }
+}
