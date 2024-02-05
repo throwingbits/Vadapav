553009990bf94463e8cfa6e31fffb44d1b7ae4c2
throwingbits <64206466+throwingbits@users.noreply.github.com>
2024-02-05 17:03:34 +0100

    Updated VadapavUriBuilder

--- src/Vadapav/Interfaces/IVadapavUriBuilder.cs
+++ src/Vadapav/Interfaces/IVadapavUriBuilder.cs
@@ -1,24 +1,10 @@
-﻿using Vadapav.Models;
-
-namespace Vadapav
+﻿namespace Vadapav
 {
     public interface IVadapavUriBuilder
     {
-        Uri GetDirectoryUri(Guid directoryId);
-        Uri GetDirectoryUri(string directoryId);
-        Uri GetDirectoryUri(VadapavDirectory directory);
-        string GetDirectoryUriString(Guid directoryId);
-        string GetDirectoryUriString(string directoryId);
-        string GetDirectoryUriString(VadapavDirectory directory);
-        Uri GetFileUri(Guid directoryId);
-        Uri GetFileUri(string fileId);
-        Uri GetFileUri(VadapavFile file);
-        string GetFileUriString(Guid fileId);
-        string GetFileUriString(string fileId);
-        string GetFileUriString(VadapavFile file);
-        Uri GetRootDirectoryUri();
-        string GetRootDirectoryUriString();
-        Uri GetSearchUri(string searchTerm);
-        string GetSearchUriString(string searchTerm);
+        Uri RootDirectoryUri { get; }
+        Uri GetUriForDirectory(string directoryId);
+        Uri GetUriForFile(string fileId);
+        Uri GetUriForSearch(string searchTerm);
     }
 }
